f9ef2a2dcbd6057168382484f031c1f1c9381c62
ayanb22 <ayanb2227@gmail.com>
2026-04-02 01:04:56 +0530

    Fibonacci Series with updates variable

--- Loop_Practice/fibonacci_series.py
+++ Loop_Practice/fibonacci_series.py
@@ -1,11 +1,11 @@
 n = int(input("Enter the number : "))
-digits_length = []
+fibonacci_series = []
 a,b = 0,1
 
 for i in range (1, n+1):
-    digits_length.append(a)
+    fibonacci_series.append(a)
     next = a + b
     a = b
     b = next
 
-print(f"The fibonacci series : {digits_length}")
+print(f"The fibonacci series : {fibonacci_series}")
