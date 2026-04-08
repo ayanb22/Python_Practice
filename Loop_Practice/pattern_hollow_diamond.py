0adedf9260d38ba572dacd6618e83a6d736ec04a
ayanb22 <ayanb2227@gmail.com>
2026-04-08 21:04:05 +0530

    Pattern Q12 : Hollow Diamond

--- Loop_Practice/pattern_hollow_diamond.py
+++ Loop_Practice/pattern_hollow_diamond.py
@@ -1,1 +1,21 @@
-n = int(input("Enter the number of rows : "))
+n = int(input("Enter the diamond size : "))
+
+for i in range(1, n+1):
+    for j in range(n-i, 0, -1):
+        print(" ", end=" ")
+    for k in range(2*i-1, 0, -1):
+        if(k==1 or k == 2*i-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+for i in range(n-1, 0, -1):
+    for j in range(n-i, 0, -1):
+        print(" ", end=" ")
+    for k in range(2*i-1, 0, -1):
+        if(k==1 or k==2*i-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
