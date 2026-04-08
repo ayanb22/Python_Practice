587cf35edbeae475f9694d06b4ae4fa2467f67aa
ayanb22 <ayanb2227@gmail.com>
2026-04-08 21:08:01 +0530

    Updated Q12

--- Loop_Practice/pattern_hollow_diamond.py
+++ Loop_Practice/pattern_hollow_diamond.py
@@ -3,7 +3,7 @@ n = int(input("Enter the diamond size : "))
 for i in range(1, n+1):
     for j in range(n-i, 0, -1):
         print(" ", end=" ")
-    for k in range(2*i-1, 0, -1):
+    for k in range(1, 2*i):
         if(k==1 or k == 2*i-1):
             print("*", end=" ")
         else:
