06cbbd171b2de635c4c238c0c9f6830dfaa907a7
ayanb22 <ayanb2227@gmail.com>
2026-04-08 20:43:22 +0530

    Pattern Q11 : Solid Diamond

---
+++ Loop_Practice/pattern_diamond.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number of rows : "))
+
+for i in range(1, n+1):
+    for j in range(n-i, 0, -1):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+        print("*", end=" ")
+    print()
+for i in range(n-1, 0, -1):
+    for j in range(n-i):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+         print("*", end=" ")
+    print()
