56b48033c5f34c8a792baade844c2cd9d12666a6
ayanb22 <ayanb2227@gmail.com>
2026-04-11 20:46:12 +0530

    Perfect Number checking with function

---
+++ Function_Practice/perfect_number_check.py
@@ -0,0 +1,15 @@
+def perfect_number(n):
+    actual_number = n
+    sum_of_divisors = 0
+
+    for i in range(1, n):
+        if(n % i == 0):
+            sum_of_divisors = sum_of_divisors + i
+    
+    return sum_of_divisors == actual_number
+
+number = int(input("Enter the number : "))
+if perfect_number(number):
+    print("This is a perfect number")
+else:
+    print("This is not a perfect number")
