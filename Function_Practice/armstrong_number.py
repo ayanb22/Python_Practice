c1bf756cf3ed105ee14d02a9d510209c22b92c94
ayanb22 <ayanb2227@gmail.com>
2026-04-14 22:05:39 +0530

    Updated armstrong number check for Edge cases

--- Function_Practice/armstrong_number.py
+++ Function_Practice/armstrong_number.py
@@ -1,17 +1,18 @@
-def armstrong_number(n):
-    m = len(n)
-    num = int(n)
-    actual_number = num
-    armstrong = 0
-    while num > 0:
-        remainder = num % 10
-        armstrong = (remainder **m) + armstrong
-        num = num // 10
+def armstrong_number(num):
+    number = abs(num)
+    m = len(str(number))
     
+    actual_number = number
+    armstrong = 0
+    while number > 0:
+        remainder = number % 10
+        armstrong = (remainder **m) + armstrong
+        number = number // 10
+
     return actual_number == armstrong
 
-number = input("Enter the number : ")
-if armstrong_number(number):
+number_input = int(input("Enter the number : "))
+if armstrong_number(number_input):
     print("This is a armstrong number")
 else:
     print("This is not an armstrong number")
