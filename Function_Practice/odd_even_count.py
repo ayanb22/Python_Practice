5301e68f57edfe08ee8c1bc94def33bd67644018
ayanb22 <ayanb2227@gmail.com>
2026-04-14 23:33:59 +0530

    updated odd & Even count for edge cases

--- Function_Practice/odd_even_count.py
+++ Function_Practice/odd_even_count.py
@@ -1,18 +1,26 @@
 def odd_even_count(n):
+    num = abs(n)
     even_count = 0
     odd_count = 0
     even_digit = []
     odd_digit = []
+    if num == 0:
+        return 1, [0], 0, []
+    
 
-    while n > 0:
-        remainder = n % 10
+    while num > 0:
+        remainder = num % 10
         if(remainder % 2 == 0):
             even_count += 1
             even_digit.append(remainder)
         else:
             odd_count += 1
             odd_digit.append(remainder)
-        n = n // 10
+        num = num // 10
+
+    even_digit.reverse()
+    odd_digit.reverse()
+    
     return even_count, even_digit, odd_count, odd_digit
 
 
