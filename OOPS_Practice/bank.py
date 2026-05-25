9a258379324a03e139b38486d81f11d86d66350f
ayanb22 <ayanb2227@gmail.com>
2026-05-25 21:36:52 +0530

    Solved a basic bank system using oops

--- OOPS_Practice/bank.py
+++ OOPS_Practice/bank.py
@@ -25,7 +25,7 @@ class Bank():
 c1 = Bank(50000, 785465)
 c1.bank_name()
 debit = int(input("Enter the amount you want to debit : "))
-c1.debit(debit)
 credit = int(input("Enter the amount you want to credit : "))
+c1.debit(debit)
 c1.credit(credit)
 c1.check_balance()
